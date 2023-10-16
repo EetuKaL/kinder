108efd4a6306721eb402066807252f6c3a862664
EetuKaL <eetukallioniemi96@gmail.com>
2023-10-16 15:27:47 +0300

    unfinished authentication

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/theme.dart';
+import 'package:frontend/pages/login_page.dart';
 import 'package:frontend/pages/suggestion_page.dart';
 import 'package:frontend/provider/card_provider.dart';
 import 'package:provider/provider.dart';
@@ -25,7 +26,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Kinder', theme: kindertheme, home: SuggestionPage());
+    return MaterialApp(title: 'Kinder', theme: kindertheme, home: LoginPage());
   }
 }
